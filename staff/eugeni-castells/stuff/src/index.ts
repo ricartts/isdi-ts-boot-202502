323050069d1543c6995b46544238a9d2e17b7550
Eugeni <ecastellsricart@gmail.com>
2025-02-27 19:12:34 +0100

    fake develop

---
+++ staff/eugeni-castells/stuff/src/index.ts
@@ -0,0 +1,11 @@
+function multiply(x: number, y: number): number {
+  return x * y;
+}
+
+multiply(2, 4);
+
+function sum(a: number, b: number): void {
+  a + b;
+}
+
+const number: number[] = [1, 2, 3];
